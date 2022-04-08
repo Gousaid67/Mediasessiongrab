feaaf484263c3bb8267f48b1e0420404ffbba063
Cage <gousaid67@gmail.com>
2022-04-08 14:31:28 +0200

    Spinner Layout

--- app/src/main/java/com/example/mediasessiongrab/MainActivity.kt
+++ app/src/main/java/com/example/mediasessiongrab/MainActivity.kt
@@ -1,22 +1,27 @@
 package com.example.mediasessiongrab
 
+import android.R
 import android.content.ComponentName
-import android.content.pm.PackageManager
 import android.graphics.drawable.Drawable
 import android.media.session.MediaController
 import android.media.session.MediaSessionManager
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.Spinner
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.chip.Chip
+
+
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(com.example.mediasessiongrab.R.layout.activity_main)
         val mngr = this.getSystemService(MEDIA_SESSION_SERVICE) as MediaSessionManager
         val mmlist = mngr.getActiveSessions(
             ComponentName(
@@ -24,13 +29,19 @@ class MainActivity : AppCompatActivity() {
                 NotificationListener::class.java
             )
         )
+       val paks = packageManager.getInstalledPackages(0)
+        for(pak in paks)
+        {
+            Log.i("", pak.packageName)
+        }
         val chips: MutableList<Chip> = mutableListOf()
 
         for(session in mmlist)
         {
 
             val chip = Chip(this)
-            val icon: Drawable = this.packageManager.getApplicationIcon(session.packageName)
+            val icon: Drawable = packageManager.getApplicationIcon(session.packageName)
+
             val txt = session.packageName
             chip.text = txt
             chip.chipIcon = icon
@@ -39,8 +50,8 @@ class MainActivity : AppCompatActivity() {
 
 
 
-        val speeen = findViewById<Spinner>(R.id.mediasessionlist)
-        val inputlist: ArrayAdapter<Chip> = ArrayAdapter<Chip>(this, android.R.layout.simple_spinner_item, chips)
+        val speeen = findViewById<Spinner>(com.example.mediasessiongrab.R.id.mediasessionlist)
+        val inputlist: ArrayAdapter<Chip> = ArrayAdapter<Chip>(this, R.layout.simple_spinner_item, chips)
         inputlist.setDropDownViewResource(com.google.android.material.R.layout.support_simple_spinner_dropdown_item)
         speeen.adapter = inputlist
 
